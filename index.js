ccd194bb326f23d7e310d939aba06f7ed779c407
Siddharth Biju <52505665+siddharthbiju@users.noreply.github.com>
2020-05-24 01:01:29 +0530

    all fixed

--- index.js
+++ index.js
@@ -1,22 +1,12 @@
 let h = document.getElementById("about").clientHeight;
-let w = document.getElementById("about").clientWidth;
-w = w * 0.6;
 h = h + 160;
 document
   .getElementById("service-background-box")
   .setAttribute("style", "height:" + h + "px");
-
 function resize() {
   h = document.getElementById("about").clientHeight;
   h = h + 160;
-  console.log("height = " + h);
-
   document
     .getElementById("service-background-box")
     .setAttribute("style", "height:" + h + "px");
-
-  console.log(
-    "set height = " +
-      document.getElementById("service-background-box").clientHeight
-  );
 }

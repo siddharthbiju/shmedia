45514fb5aa3dc7448d389fffae476a3e651bdf3a
Siddharth Biju <52505665+siddharthbiju@users.noreply.github.com>
2020-05-24 00:50:26 +0530

    heightfix

--- index.js
+++ index.js
@@ -6,20 +6,17 @@ document
   .getElementById("service-background-box")
   .setAttribute("style", "height:" + h + "px");
 
-document
-  .getElementById("service-background-box")
-  .setAttribute("style", "width:" + w + "px");
-
 function resize() {
   h = document.getElementById("about").clientHeight;
   h = h + 160;
-  w = document.getElementById("about").clientWidth;
-  w = w * 0.6;
+  console.log("height = " + h);
 
   document
     .getElementById("service-background-box")
     .setAttribute("style", "height:" + h + "px");
-  document
-    .getElementById("service-background-box")
-    .setAttribute("style", "width:" + w + "px");
+
+  console.log(
+    "set height = " +
+      document.getElementById("service-background-box").clientHeight
+  );
 }

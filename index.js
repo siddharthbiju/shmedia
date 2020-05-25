c078bfd9265e73a60f187754b204de97657afef4
Siddharth Biju <52505665+siddharthbiju@users.noreply.github.com>
2020-05-25 19:21:55 +0530

    yt mobile

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
 let h = document.getElementById("about").clientHeight;
 let conh = document.getElementById("get-contact").clientHeight;
+let mobileyt = document.getElementById("mobile-yt").clientWidth;
+mobileyt = (mobileyt * 9) / 16;
 h = h + 160;
 conh = conh + 160;
-
 document
   .getElementById("service-background-box")
   .setAttribute("style", "height:" + h + "px");
 document
   .getElementById("get-contact-background-box")
   .setAttribute("style", "height:" + conh + "px");
+
+document
+  .getElementById("mobile-yt")
+  .setAttribute("style", "height:" + mobileyt + "px");
 function resize() {
   h = document.getElementById("about").clientHeight;
   conh = document.getElementById("get-contact").clientHeight;
+  mobileyt = document.getElementById("mobile-yt").clientWidth;
   h = h + 160;
   conh = conh + 160;
+  mobileyt = (mobileyt * 9) / 16;
   document
     .getElementById("service-background-box")
     .setAttribute("style", "height:" + h + "px");
-  console.log(conh);
   document
     .getElementById("get-contact-background-box")
     .setAttribute("style", "height:" + conh + "px");
+  document
+    .getElementById("mobile-yt")
+    .setAttribute("style", "height:" + mobileyt + "px");
 }

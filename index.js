4ab1358196f531abd856ef583c4fb934f7f9c038
Siddharth Biju <52505665+siddharthbiju@users.noreply.github.com>
2020-05-25 18:35:22 +0530

    change

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 let h = document.getElementById("about").clientHeight;
+let conh = document.getElementById("get-contact").clientHeight;
 h = h + 160;
+conh = conh + 160;
+
 document
   .getElementById("service-background-box")
   .setAttribute("style", "height:" + h + "px");
+document
+  .getElementById("get-contact-background-box")
+  .setAttribute("style", "height:" + conh + "px");
 function resize() {
   h = document.getElementById("about").clientHeight;
+  conh = document.getElementById("get-contact").clientHeight;
   h = h + 160;
+  conh = conh + 160;
   document
     .getElementById("service-background-box")
     .setAttribute("style", "height:" + h + "px");
+  console.log(conh);
+  document
+    .getElementById("get-contact-background-box")
+    .setAttribute("style", "height:" + conh + "px");
 }

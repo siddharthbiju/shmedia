050c0e3552b77100c7e1d26d05a062d56d9566c2
Siddharth Biju <52505665+siddharthbiju@users.noreply.github.com>
2020-05-27 23:45:55 +0530

    dropdown on click

--- index.js
+++ index.js
@@ -5,29 +5,48 @@ mobileyt = (mobileyt * 9) / 16;
 h = h + 160;
 conh = conh + 160;
 document
-  .getElementById("service-background-box")
-  .setAttribute("style", "height:" + h + "px");
-document
-  .getElementById("get-contact-background-box")
-  .setAttribute("style", "height:" + conh + "px");
-
-document
-  .getElementById("mobile-yt")
-  .setAttribute("style", "height:" + mobileyt + "px");
-function resize() {
-  h = document.getElementById("about").clientHeight;
-  conh = document.getElementById("get-contact").clientHeight;
-  mobileyt = document.getElementById("mobile-yt").clientWidth;
-  h = h + 160;
-  conh = conh + 160;
-  mobileyt = (mobileyt * 9) / 16;
-  document
     .getElementById("service-background-box")
     .setAttribute("style", "height:" + h + "px");
-  document
+document
     .getElementById("get-contact-background-box")
     .setAttribute("style", "height:" + conh + "px");
-  document
+
+document
     .getElementById("mobile-yt")
     .setAttribute("style", "height:" + mobileyt + "px");
+
+function resize() {
+    h = document.getElementById("about").clientHeight;
+    conh = document.getElementById("get-contact").clientHeight;
+    mobileyt = document.getElementById("mobile-yt").clientWidth;
+    h = h + 160;
+    conh = conh + 160;
+    mobileyt = (mobileyt * 9) / 16;
+    document
+        .getElementById("service-background-box")
+        .setAttribute("style", "height:" + h + "px");
+    document
+        .getElementById("get-contact-background-box")
+        .setAttribute("style", "height:" + conh + "px");
+    document
+        .getElementById("mobile-yt")
+        .setAttribute("style", "height:" + mobileyt + "px");
 }
+
+$(".main-body").on("click", function() {
+    $(".dropdown").children(".dropdown-menu").slideUp(300);
+
+    if ($(this).parent().hasClass("open")) {
+        $(this).next(".dropdown-menu").slideUp(300);
+    }
+});
+
+$(".navbar-nav a").on("click", function() {
+    $(".dropdown").children(".dropdown-menu").slideUp(300);
+
+    if ($(this).parent().hasClass("open")) {
+        $(this).next(".dropdown-menu").slideUp(300);
+    } else {
+        $(this).next(".dropdown-menu").slideDown(300);
+    }
+});

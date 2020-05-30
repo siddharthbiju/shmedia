a30e450fe9603991cf066002f711545528ac2ccb
Siddharth Biju <52505665+siddharthbiju@users.noreply.github.com>
2020-05-30 12:18:27 +0530

    change

--- index.js
+++ index.js
@@ -2,61 +2,83 @@ let h = document.getElementById("about").clientHeight;
 let conh = document.getElementById("get-contact").clientHeight;
 let mobileyt = document.getElementById("mobile-yt").clientWidth;
 let pcytheight = document.getElementById("slider-wrapper").clientHeight;
+let aboutWidth = document.getElementById("about").clientHeight;
+aboutWidth = aboutWidth * 2;
+console.log(aboutWidth);
+
 mobileyt = (mobileyt * 9) / 16;
 h = h + 160;
 aboutImageHeight = h - 40;
 conh = conh + 160;
 document
+  .getElementById("service-background-box")
+  .setAttribute("style", "height:" + h + "px");
+document
+  .getElementById("get-contact-background-box")
+  .setAttribute("style", "height:" + conh + "px");
+
+document
+  .getElementById("mobile-yt")
+  .setAttribute("style", "height:" + mobileyt + "px");
+
+document
+  .getElementById("comments-section")
+  .setAttribute("style", "height:" + pcytheight + "px");
+
+document
+  .getElementById("about-images")
+  .setAttribute("style", "height:" + aboutImageHeight + "px");
+
+document
+  .getElementById("about-images-2")
+  .setAttribute("style", "height:" + aboutImageHeight + "px");
+
+document
+  .getElementById("about-images-container")
+  .setAttribute("style", "width:" + aboutWidth + "px");
+
+function resize() {
+  h = document.getElementById("about").clientHeight;
+  conh = document.getElementById("get-contact").clientHeight;
+  mobileyt = document.getElementById("mobile-yt").clientWidth;
+  pcytheight = document.getElementById("slider-wrapper").clientHeight;
+  aboutWidth = document.getElementById("about").clientHeight;
+  aboutWidth = aboutWidth * 2;
+  h = h + 160;
+  aboutImageHeight = h - 40;
+  conh = conh + 160;
+  mobileyt = (mobileyt * 9) / 16;
+  document
     .getElementById("service-background-box")
     .setAttribute("style", "height:" + h + "px");
-document
+  document
     .getElementById("get-contact-background-box")
     .setAttribute("style", "height:" + conh + "px");
-
-document
+  document
     .getElementById("mobile-yt")
     .setAttribute("style", "height:" + mobileyt + "px");
-
-document
+  document
     .getElementById("comments-section")
     .setAttribute("style", "height:" + pcytheight + "px");
-
-document
+  document
     .getElementById("about-images")
     .setAttribute("style", "height:" + aboutImageHeight + "px");
 
-document
+  document
     .getElementById("about-images-2")
     .setAttribute("style", "height:" + aboutImageHeight + "px");
 
-function resize() {
-    h = document.getElementById("about").clientHeight;
-    conh = document.getElementById("get-contact").clientHeight;
-    mobileyt = document.getElementById("mobile-yt").clientWidth;
-    pcytheight = document.getElementById("slider-wrapper").clientHeight;
-    h = h + 160;
-    conh = conh + 160;
-    mobileyt = (mobileyt * 9) / 16;
-    document
-        .getElementById("service-background-box")
-        .setAttribute("style", "height:" + h + "px");
-    document
-        .getElementById("get-contact-background-box")
-        .setAttribute("style", "height:" + conh + "px");
-    document
-        .getElementById("mobile-yt")
-        .setAttribute("style", "height:" + mobileyt + "px");
-    document
-        .getElementById("comments-section")
-        .setAttribute("style", "height:" + pcytheight + "px");
+  document
+    .getElementById("about-images-container")
+    .setAttribute("style", "width:" + aboutWidth + "px");
 }
 if ($(window).width() > 1024) {
-    $(".navbar .dropdown").hover(
-        function () {
-            $(this).find(".dropdown-menu").first().stop(true, true).slideDown(300);
-        },
-        function () {
-            $(this).find(".dropdown-menu").first().stop(true, true).slideUp(300);
-        }
-    );
+  $(".navbar .dropdown").hover(
+    function () {
+      $(this).find(".dropdown-menu").first().stop(true, true).slideDown(300);
+    },
+    function () {
+      $(this).find(".dropdown-menu").first().stop(true, true).slideUp(300);
+    }
+  );
 }

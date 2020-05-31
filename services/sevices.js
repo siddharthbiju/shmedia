bf1235b7dd56c99b2c092a4e93fe368decf6e3d2
Siddharth Biju <52505665+siddharthbiju@users.noreply.github.com>
2020-05-31 17:54:41 +0530

    services

---
+++ services/sevices.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    $(".dropdown").hover(
+        function () {
+            $("dropdown-images.dropdown-menu", this)
+                .stop(true, true)
+                .slideDown("fast");
+            $(this).addClass("open");
+        },
+        function () {
+            $("dropdown-images.dropdown-menu", this).stop(true, true).slideUp("fast");
+            $(this).removeClass("open");
+        }
+    );
+});

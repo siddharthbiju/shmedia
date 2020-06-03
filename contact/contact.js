42b44e1065e0e0c65b5900830e6f3bdbc9c816d5
Siddharth Biju <52505665+siddharthbiju@users.noreply.github.com>
2020-06-03 11:24:24 +0530

    contact

---
+++ contact/contact.js
@@ -0,0 +1,46 @@
+$(document).ready(function () {
+    $(".dropdown").hover(
+        function () {
+            $("dropdown-images.dropdown-menu", this)
+                .stop(true, true)
+                .slideDown("fast");
+            $(this).addClass("open");
+        },
+        function () {
+            $("dropdown-images.dropdown-menu", this).stop(true, true).slideUp("fast");
+            $(this).removeClass("open");
+        }
+    );
+});
+
+
+let mobileyt = document.getElementById("mobile-yt").clientWidth;
+let conh = document.getElementById("get-contact").clientHeight;
+conh = conh + 160;
+
+document
+    .getElementById("get-contact-background-box")
+    .setAttribute("style", "height:" + conh + "px");
+
+
+
+mobileyt = (mobileyt * 9) / 16;
+document
+    .getElementById("mobile-yt")
+    .setAttribute("style", "height:" + mobileyt + "px");
+
+function resize() {
+    let mobileyt = document.getElementById("mobile-yt").clientWidth;
+    mobileyt = (mobileyt * 9) / 16;
+    conh = document.getElementById("get-contact").clientHeight;
+    conh = conh + 160;
+
+
+    document
+        .getElementById("mobile-yt")
+        .setAttribute("style", "height:" + mobileyt + "px");
+
+    document
+        .getElementById("get-contact-background-box")
+        .setAttribute("style", "height:" + conh + "px");
+}

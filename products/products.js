69091458f865a1a266785ffdf0f0b56fb4ae979c
Siddharth Biju <52505665+siddharthbiju@users.noreply.github.com>
2020-06-08 21:08:40 +0530

    url not started

---
+++ products/products.js
@@ -0,0 +1,56 @@
+$(document).ready(function () {
+    $(".dropdown").hover(
+        function () {
+            $("dropdown-images.dropdown-menu", this)
+                .stop(true, true)
+                .slideDown("fast");
+            $(this).addClass("open");
+        },
+        function () {
+            $("dropdown-images.dropdown-menu", this).stop(true, true).slideUp("fast");
+            $(this).removeClass("open");
+        }
+    );
+});
+
+
+let mobileyt = document.getElementById("mobile-yt").clientWidth;
+mobileyt = (mobileyt * 9) / 16;
+document
+    .getElementById("mobile-yt")
+    .setAttribute("style", "height:" + mobileyt + "px");
+
+function resize() {
+    let mobileyt = document.getElementById("mobile-yt").clientWidth;
+    mobileyt = (mobileyt * 9) / 16;
+    document
+        .getElementById("mobile-yt")
+        .setAttribute("style", "height:" + mobileyt + "px");
+}
+
+const element5 = document.getElementById("gotop");
+let scrolltotop = 0;
+
+$.fn.isInViewport = function () {
+    var elementTop = $(this).offset().top;
+    var elementBottom = elementTop + $(this).outerHeight();
+
+    var viewportTop = $(window).scrollTop();
+    var viewportBottom = viewportTop + $(window).height();
+
+    return elementBottom > viewportTop && elementTop < viewportBottom;
+};
+
+$(window).on('resize scroll', function () {
+    if ($('.hero-title').isInViewport()) {
+        if (scrolltotop == 1) {
+            element5.classList.remove("slide-top-top");
+            element5.classList.add("slide-out-bottom");
+        }
+    } else {
+        element5.classList.remove("slide-out-bottom");
+        element5.classList.add("slide-top-top");
+        scrolltotop = 1;
+    }
+
+});

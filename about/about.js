b739f32b42e593e13bb084a4fcaaae35abbb19e2
Siddharth Biju <52505665+siddharthbiju@users.noreply.github.com>
2020-06-14 12:36:44 +0530

    gallery

--- about/about.js
+++ about/about.js
@@ -1,100 +1,104 @@
 $(document).ready(function () {
-    $(".dropdown").hover(
-        function () {
-            $("dropdown-images.dropdown-menu", this)
-                .stop(true, true)
-                .slideDown("fast");
-            $(this).addClass("open");
-        },
-        function () {
-            $("dropdown-images.dropdown-menu", this).stop(true, true).slideUp("fast");
-            $(this).removeClass("open");
-        }
-    );
+  $(".dropdown").hover(
+    function () {
+      $("dropdown-images.dropdown-menu", this)
+        .stop(true, true)
+        .slideDown("fast");
+      $(this).addClass("open");
+    },
+    function () {
+      $("dropdown-images.dropdown-menu", this).stop(true, true).slideUp("fast");
+      $(this).removeClass("open");
+    }
+  );
 });
-
 
 let mobileyt = document.getElementById("mobile-yt").clientWidth;
 mobileyt = (mobileyt * 9) / 16;
 document
-    .getElementById("mobile-yt")
-    .setAttribute("style", "height:" + mobileyt + "px");
+  .getElementById("mobile-yt")
+  .setAttribute("style", "height:" + mobileyt + "px");
 
 function resize() {
-    let mobileyt = document.getElementById("mobile-yt").clientWidth;
-    mobileyt = (mobileyt * 9) / 16;
-    document
-        .getElementById("mobile-yt")
-        .setAttribute("style", "height:" + mobileyt + "px");
+  let mobileyt = document.getElementById("mobile-yt").clientWidth;
+  mobileyt = (mobileyt * 9) / 16;
+  document
+    .getElementById("mobile-yt")
+    .setAttribute("style", "height:" + mobileyt + "px");
 }
-
 
 let h = document.getElementById("about").clientHeight;
 let about_backgroud_width = document.getElementById("about").clientWidth;
 
 h = h + 160;
-about_backgroud_width = about_backgroud_width * .7
+about_backgroud_width = about_backgroud_width * 0.7;
 aboutImageHeight = h - 40;
 
 document.getElementById("service-background-box").style.height = h + "px";
-document.getElementById("service-background-box").style.width = about_backgroud_width + "px";
+document.getElementById("service-background-box").style.width =
+  about_backgroud_width + "px";
 
-let about_images_width = document.getElementById("service-background-box").clientWidth;
+let about_images_width = document.getElementById("service-background-box")
+  .clientWidth;
 about_images_width = about_images_width + 804;
 
-document.getElementById("about-images-container").style.width = about_images_width + "px";
+document.getElementById("about-images-container").style.width =
+  about_images_width + "px";
 document.getElementById("about-images").style.height = aboutImageHeight + "px";
-document.getElementById("about-images-2").style.height = aboutImageHeight + "px";
+document.getElementById("about-images-2").style.height =
+  aboutImageHeight + "px";
 
 function resize() {
-    h = document.getElementById("about").clientHeight;
-    about_backgroud_width = document.getElementById("about").clientWidth;
+  h = document.getElementById("about").clientHeight;
+  about_backgroud_width = document.getElementById("about").clientWidth;
 
-    h = h + 160;
-    about_backgroud_width = about_backgroud_width * .7
-    aboutImageHeight = h - 40;
+  h = h + 160;
+  about_backgroud_width = about_backgroud_width * 0.7;
+  aboutImageHeight = h - 40;
 
-    document.getElementById("service-background-box").style.height = h + "px";
-    document.getElementById("service-background-box").style.width = about_backgroud_width + "px";
+  document.getElementById("service-background-box").style.height = h + "px";
+  document.getElementById("service-background-box").style.width =
+    about_backgroud_width + "px";
 
-    about_images_width = document.getElementById("service-background-box").clientWidth;
-    about_images_width = about_images_width + 804;
+  about_images_width = document.getElementById("service-background-box")
+    .clientWidth;
+  about_images_width = about_images_width + 804;
 
-    document.getElementById("about-images-container").style.width = about_images_width + "px";
-    document.getElementById("about-images").style.height = aboutImageHeight + "px";
-    document.getElementById("about-images-2").style.height = aboutImageHeight + "px";
-
+  document.getElementById("about-images-container").style.width =
+    about_images_width + "px";
+  document.getElementById("about-images").style.height =
+    aboutImageHeight + "px";
+  document.getElementById("about-images-2").style.height =
+    aboutImageHeight + "px";
 }
 
 function serviceClick() {
-    console.log("clicked");
-    window.location.href = "/services/services.html";
+  console.log("clicked");
+  // window.location.href = "/services/services.html";
 }
-
 
 const element5 = document.getElementById("gotop");
 let scrolltotop = 0;
 
 $.fn.isInViewport = function () {
-    var elementTop = $(this).offset().top;
-    var elementBottom = elementTop + $(this).outerHeight();
+  var elementTop = $(this).offset().top;
+  var elementBottom = elementTop + $(this).outerHeight();
 
-    var viewportTop = $(window).scrollTop();
-    var viewportBottom = viewportTop + $(window).height();
+  var viewportTop = $(window).scrollTop();
+  var viewportBottom = viewportTop + $(window).height();
 
-    return elementBottom > viewportTop && elementTop < viewportBottom;
+  return elementBottom > viewportTop && elementTop < viewportBottom;
 };
 
-$(window).on('resize scroll', function () {
-    if ($('.hero-title').isInViewport()) {
-        if (scrolltotop == 1) {
-            element5.classList.remove("slide-top-top");
-            element5.classList.add("slide-out-bottom");
-        }
-    } else {
-        element5.classList.remove("slide-out-bottom");
-        element5.classList.add("slide-top-top");
-        scrolltotop = 1;
+$(window).on("resize scroll", function () {
+  if ($(".hero-title").isInViewport()) {
+    if (scrolltotop == 1) {
+      element5.classList.remove("slide-top-top");
+      element5.classList.add("slide-out-bottom");
     }
-
+  } else {
+    element5.classList.remove("slide-out-bottom");
+    element5.classList.add("slide-top-top");
+    scrolltotop = 1;
+  }
 });
--- gallery/gallery.js
+++ gallery/gallery.js
@@ -1,67 +1,57 @@
 $(document).ready(function () {
-    $(".dropdown").hover(
-        function () {
-            $("dropdown-images.dropdown-menu", this)
-                .stop(true, true)
-                .slideDown("fast");
-            $(this).addClass("open");
-        },
-        function () {
-            $("dropdown-images.dropdown-menu", this).stop(true, true).slideUp("fast");
-            $(this).removeClass("open");
-        }
-    );
+  $(".dropdown").hover(
+    function () {
+      $("dropdown-images.dropdown-menu", this)
+        .stop(true, true)
+        .slideDown("fast");
+      $(this).addClass("open");
+    },
+    function () {
+      $("dropdown-images.dropdown-menu", this).stop(true, true).slideUp("fast");
+      $(this).removeClass("open");
+    }
+  );
 });
-
 
 let mobileyt = document.getElementById("mobile-yt").clientWidth;
 
-
-
 mobileyt = (mobileyt * 9) / 16;
 document
+  .getElementById("mobile-yt")
+  .setAttribute("style", "height:" + mobileyt + "px");
+
+function resize() {
+  let mobileyt = document.getElementById("mobile-yt").clientWidth;
+  mobileyt = (mobileyt * 9) / 16;
+
+  document
     .getElementById("mobile-yt")
     .setAttribute("style", "height:" + mobileyt + "px");
-
-function resize() {
-    let mobileyt = document.getElementById("mobile-yt").clientWidth;
-    mobileyt = (mobileyt * 9) / 16;
-
-    document
-        .getElementById("mobile-yt")
-        .setAttribute("style", "height:" + mobileyt + "px");
-
-
 }
-
 
 function serviceClick() {
-    console.log("clicked");
-    window.location.href = "/services/services.html";
+  //  window.location.href = "/services/services.html";
 }
 
+let image_height = document.getElementById("image_height").clientWidth;
 
-let image_height = document.getElementById("image_height").clientHeight;
+image_height = image_height / 2;
+image_height = image_height - 75;
 
-image_height = (image_height * 2) - 50;
-image_height = 0 - image_height;
-
-var f_images = document.getElementsByClassName("img-1");
+var f_images = document.getElementsByClassName("full_screen");
 var i;
 for (i = 0; i < 9; i++) {
-    f_images[i].style.marginTop = image_height + "px";
+  f_images[i].style.marginLeft = image_height + "px";
 }
 
 function resize() {
-    image_height = document.getElementById("image_height").clientHeight;
-
-    image_height = (image_height * 2) - 50;
-    image_height = 0 - image_height;
-    for (i = 0; i < 9; i++) {
-        f_images[i].style.marginTop = image_height + "px";
-    }
+  image_height = document.getElementById("image_height").clientWidth;
+  image_height = image_height / 2;
+  image_height = image_height - 75;
+  for (i = 0; i < 9; i++) {
+    f_images[i].style.marginLeft = image_height + "px";
+  }
 }
-
 
 // Get the modal
 var modal = document.getElementById("myModal");
@@ -73,82 +63,79 @@ var captionText = document.getElementById("caption");
 i = 0;
 var f_src = document.getElementsByClassName("img-2");
 
-
 f_images[0].onclick = function () {
-    modal.style.display = "block";
-    modalImg.src = f_src[0].src;
-}
+  modal.style.display = "block";
+  modalImg.src = f_src[0].src;
+};
 
 f_images[1].onclick = function () {
-    modal.style.display = "block";
-    modalImg.src = f_src[1].src;
-}
+  modal.style.display = "block";
+  modalImg.src = f_src[1].src;
+};
 
 f_images[2].onclick = function () {
-    modal.style.display = "block";
-    modalImg.src = f_src[2].src;
-}
+  modal.style.display = "block";
+  modalImg.src = f_src[2].src;
+};
 
 f_images[3].onclick = function () {
-    modal.style.display = "block";
-    modalImg.src = f_src[3].src;
-}
+  modal.style.display = "block";
+  modalImg.src = f_src[3].src;
+};
 
 f_images[4].onclick = function () {
-    modal.style.display = "block";
-    modalImg.src = f_src[4].src;
-}
+  modal.style.display = "block";
+  modalImg.src = f_src[4].src;
+};
 
 f_images[5].onclick = function () {
-    modal.style.display = "block";
-    modalImg.src = f_src[5].src;
-}
+  modal.style.display = "block";
+  modalImg.src = f_src[5].src;
+};
 
 f_images[6].onclick = function () {
-    modal.style.display = "block";
-    modalImg.src = f_src[6].src;
-}
+  modal.style.display = "block";
+  modalImg.src = f_src[6].src;
+};
 
 f_images[7].onclick = function () {
-    modal.style.display = "block";
-    modalImg.src = f_src[7].src;
-}
+  modal.style.display = "block";
+  modalImg.src = f_src[7].src;
+};
 
 f_images[8].onclick = function () {
-    modal.style.display = "block";
-    modalImg.src = f_src[8].src;
-}
-
+  modal.style.display = "block";
+  modalImg.src = f_src[8].src;
+};
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 span.onclick = function () {
-    modal.style.display = "none";
-}
+  modal.style.display = "none";
+};
 
 const element5 = document.getElementById("gotop");
 let scrolltotop = 0;
 
 $.fn.isInViewport = function () {
-    var elementTop = $(this).offset().top;
-    var elementBottom = elementTop + $(this).outerHeight();
+  var elementTop = $(this).offset().top;
+  var elementBottom = elementTop + $(this).outerHeight();
 
-    var viewportTop = $(window).scrollTop();
-    var viewportBottom = viewportTop + $(window).height();
+  var viewportTop = $(window).scrollTop();
+  var viewportBottom = viewportTop + $(window).height();
 
-    return elementBottom > viewportTop && elementTop < viewportBottom;
+  return elementBottom > viewportTop && elementTop < viewportBottom;
 };
 
-$(window).on('resize scroll', function () {
-    if ($('.hero-title').isInViewport()) {
-        if (scrolltotop == 1) {
-            element5.classList.remove("slide-top-top");
-            element5.classList.add("slide-out-bottom");
-        }
-    } else {
-        element5.classList.remove("slide-out-bottom");
-        element5.classList.add("slide-top-top");
-        scrolltotop = 1;
+$(window).on("resize scroll", function () {
+  if ($(".hero-title").isInViewport()) {
+    if (scrolltotop == 1) {
+      element5.classList.remove("slide-top-top");
+      element5.classList.add("slide-out-bottom");
     }
-
+  } else {
+    element5.classList.remove("slide-out-bottom");
+    element5.classList.add("slide-top-top");
+    scrolltotop = 1;
+  }
 });

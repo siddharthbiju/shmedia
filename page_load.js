d9c146efc63d7f8a819b915931277e44dd950f58
Siddharth Biju <52505665+siddharthbiju@users.noreply.github.com>
2020-06-25 14:19:05 +0530

    mobile view

--- page_load.js
+++ page_load.js
@@ -1,4 +1,7 @@
-left = screen.width / 2 - 236;
 
-const element47 = document.getElementById("load_img");
-element47.style.marginLeft = left + "px";
+if (screen.width > 600) {
+    left = screen.width / 2 - 236;
+
+    const element47 = document.getElementById("load_img");
+    element47.style.marginLeft = left + "px";
+}

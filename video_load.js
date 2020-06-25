ac9fb8e427fa0590e0c50b30eab8ded72130af76
Siddharth Biju <52505665+siddharthbiju@users.noreply.github.com>
2020-06-25 11:51:28 +0530

    ipad fix

--- video_load.js
+++ video_load.js
@@ -1,5 +1,5 @@
 var video_src = document.getElementById("video-bg");
-if (screen.width > 600) {
+if (screen.width > 800) {
     video_src.src = "/videos/FinalComp.mp4";
 } else {
     video_src.src = "/videos/output.mp4";

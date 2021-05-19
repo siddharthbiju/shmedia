d0a09657f05d895f403c3ff9cb82029980ac827f
Siddharth Biju <siddharthbiju@gmail.com>
2021-05-19 14:45:27 +0530

    seo

--- mail_handler.php
+++ mail_handler.php
@@ -1,26 +1,29 @@
 <link rel="stylesheet" href="php.css" type="text/css">
 <?php
 
-$name = $_POST['name'];
-$email = $_POST['email'];
-$phone = $_POST['phone'];
-$msg = $email;
+		$name=$_POST['name'];
+		$email=$_POST['email'];
+		$phone=$_POST['phone'];
+		$msg= $email;
 
-$to = 'srth.raj@gmail.com'; // Receiver Email ID, Replace with your email ID
-$subject = 'New reply from shdigimedia.com';
-$message = "Name : " . $name . "<br />" . "Phone : " . $phone . "<br />" . "Email : " . $msg;
+		// Receiver Email ID, Replace with your email ID
+		$subject='New reply from shdigimedia.com';
+		$message="Name : ".$name."<br />"."Phone : ".$phone."<br />"."Email : ".$msg;
 
-$headers = "MIME-Version: 1.0" . "\r\n";
-$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+		$headers = "MIME-Version: 1.0" . "\r\n";
+		$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 
-$headers .= 'From: <website@shdigimedia.com>' . "\r\n";
-
-if (mail($to, $subject, $message, $headers)) {
-    echo "<p>We will contact you shortly!</p>";
-    $to = 'info@shdigimedia.com';
-    mail($to, $subject, $message, $headers);
-} else {
-    echo "<p>Something went wrong!</p>";
-}
-
+		$headers .= 'From: <website@shdigimedia.com>' . "\r\n";
+		
+		$to='info@shdigimedia.com';
+		mail($to, $subject, $message, $headers);
+        
+        $to='rahulmr@shdigimedia.com'; 
+		if(mail($to, $subject, $message, $headers)){
+			echo "<p>We will contact you shortly!</p>";
+		}
+		else{
+			echo "<p>Something went wrong!</p>";
+		}
+	
 ?>
